49b3af906000a56722181517ded302a00f7c14ae
BrooksPoltl <bpoltl1@gmail.com>
2019-06-17 15:38:45 -0500

    finished test

--- test/test.js
+++ test/test.js
@@ -48,4 +48,73 @@ describe('Testing Compound interest', function() {
         assert.deepEqual([0,36500,73000,109500,146000,182500],result.principal);
     });
   });
+  describe('period = 1 initial = 1000', function() {
+    const result = compound(1000,100,5,.1)
+    
+    it('should return correct result', function() {
+      assert.equal(result.result, 2221.02);
+    });
+    it('should return correct array of total', function() {
+        assert.deepEqual([1000, 1200,1420, 1662, 1928.2, 2221.02],result.total);
+    });
+    it('should return correct array of interest', function() {
+        assert.deepEqual([0, 100, 220, 362, 528.2, 721.02], result.interest);
+    });
+    it('should return correct array of principal', function() {
+        assert.deepEqual([1000,1100,1200,1300,1400,1500], result.principal);
+    });
+  });
+  describe('period = 12 initial = 1000', function() {
+    const result = compound(1000,100,5,.1,12)
+    it('should return correct result', function() {
+        assert.equal(9389.02,result.result);
+    });
+    it('should return correct array of total', function() {
+        assert.deepEqual([1000,2361.27,3865.08, 5526.36, 7361.6, 9389.02],result.total);
+    });
+    it('should return correct array of interest', function() {
+        assert.deepEqual([0, 161.27, 465.08, 926.36, 1561.6, 2389.02], result.interest);
+    });
+    it('should return correct array of principal', function() {
+        assert.deepEqual([1000,2200,3400,4600,5800,7000], result.principal);
+    });
+  });
+  describe('period = 365 initial = 1000', function() {
+    const result = compound(1000,100,5,.1,365)
+
+    it('should return correct result', function() {
+        assert.equal(238390.66,result.result);
+      });
+    it('should return correct array of total', function() {
+        assert.deepEqual([1000,39487.02,82021.16,129028.02, 180977.93, 238390.66],result.total);
+    });
+    it('should return correct array of interest', function() {
+        assert.deepEqual([0, 1987.02, 8021.16, 18528.02, 33977.93, 54890.66],result.interest);
+    });
+    it('should return correct array of principal', function() {
+        assert.deepEqual([1000,37500,74000,110500,147000,183500],result.principal);
+    });
+  });
+  describe('takes string arguments', function(){
+      it('should take initial string',function(){
+        const result = compound('1000',100,5,.1,12)
+        assert.equal(9389.02,result.result);
+      })
+      it ('should take amount string',function(){
+        const result = compound(1000,'100',5,.1,12)
+        assert.equal(9389.02,result.result);
+      })
+      it('should take year string',function(){
+        const result = compound(1000,100,'5',.1,12)
+        assert.equal(9389.02,result.result);
+      })
+      it ('should take interest string',function(){
+        const result = compound(1000,100,5,'.1',12)
+        assert.equal(9389.02,result.result);
+      })
+      it ('should take period string', function(){
+        const result = compound(1000,100,5,.1,'12')
+        assert.equal(9389.02,result.result);
+      })
+  })
 });
